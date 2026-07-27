8ea02c22af600a2c1e6aca2678062968161c170b
Rajapandi <rp6321613@gmail.com>
2026-07-27 12:44:01 +0530

    new

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-state-2909"
-    key     = "dev/vpc/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket = "my-terraform-state-2909"
+    key    = "dev/vpc/terraform.tfstate"
+    region = "us-east-1"
   }
 }
